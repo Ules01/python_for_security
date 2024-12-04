efc65b07a9759cb0e68947e898072ef4fa2a18dd
AzzaMani <azzamani545@gmail.com>
2024-12-04 13:00:11 +0100

    log results

--- script_attack/portscan.py
+++ script_attack/portscan.py
@@ -2,18 +2,19 @@ from pwn import *
 
 
 def portscan_attack(ip, start_port, end_port):
-    for port in range(start_port, end_port):
-        try:
-            target = remote(ip, port)
-            print(f"Port {port} is open.")
-            target.send(b"GET / HTTP/1.1\r\nHost: localhost\r\n\r\n")
-            #print(target.recvuntil(b"Simulated vulnerable server").decode())
-            target.close()
-        except Exception as e:
-            print(f"Port {port} is closed or unreacheble. Error: {e}")
+    with open("attack_log", "+a") as f:
+        f.write(f"Port Scan Results for {ip} (Ports Range {start_port} -- {end_port}):\n")
+        for port in range(start_port, end_port):
+            try:
+                target = remote(ip, port)
+                f.write(f"Port {port} is open.\n")
+                target.send(b"GET / HTTP/1.1\r\nHost: localhost\r\n\r\n")
+                target.close()
+            except Exception as e:
+                continue
 
 if __name__ == "__main__":
     ip = "localhost"
     start_port = 70
-    end_port = 100
+    end_port = 400
     portscan_attack(ip, start_port, end_port)    
