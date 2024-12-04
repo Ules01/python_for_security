3f7609183aeb6012327e70a3f31fb29572725331
Jules LEROQUAIS <leroquais.jules@gmail.com>
2024-12-04 10:37:21 +0100

    first logs

--- honeypots/bruteforce_honeypot/script.py
+++ honeypots/bruteforce_honeypot/script.py
@@ -31,4 +31,4 @@ def login():
     return "Access Denied", 401
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=22)
+    app.run(host="0.0.0.0", port=3000)
--- launch_honey.sh
+++ launch_honey.sh
@@ -2,10 +2,14 @@
 
 cd honeypots
 sudo docker build -t honeypots .
-sudo docker run -d -p 8080:8080 -p 22:22 -p 80:80 \
-    -v $(pwd)/logs:/honeypots/logs honeypots
+sudo docker run -d -p 8080:8080 -p 3000:3000 -p 80:80 \
+    -v $(pwd)/logs:/app/logs honeypots
 
-echo "################################################"
-echo Honey pots lauched with Success
-echo "################################################"
+if [ $? -eq 0 ]; then
+    echo "################################################"
+    echo Honey pots lauched with Success
+    echo "################################################"
+    exit 0
+fi
+exit 1
 
